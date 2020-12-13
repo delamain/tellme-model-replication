9dfb74b4d3d483efc4ce1091c73c54e95af23daf
delamain <henry.delamain@gmail.com>
2020-12-13 10:13:51 +0000

    Added in SEIR global variables

--- Model.py
+++ Model.py
@@ -5,7 +5,7 @@ the environment and reads off values.
 """
 from panaxea.core.Environment import ObjectGrid, ObjectGrid2D, NumericalGrid2D, Grid2D
 from panaxea.core.Model import Model
-from panaxea.core.Steppables import Agent
+from panaxea.core.Steppables import Agent, Helper
 import random
 import Region
 
@@ -15,13 +15,12 @@ class SimpleAgent(Agent):
         super(SimpleAgent, self).__init__()
         randomX = self.random_xposition_within_grid(model)
         randomY = self.random_yposition_within_grid(model)
-        numerical_env.grid[(randomX, randomY)] += 1
+        #numerical_env.grid[(randomX, randomY)] += 1
 
         # automatically adds agent to the environment
         self.add_agent_to_grid("agent_env", (randomX, randomY), model)
         model.environments["agent_env"].add_agent_to_patch(randomX, randomY)
 
-        print("Num of patches", model.environments["agent_env"].number_of_patches())
         self.end_of_grid = False
 
     def random_xposition_within_grid(self, model):
@@ -34,59 +33,49 @@ class SimpleAgent(Agent):
 
     def step_main(self, model):
         current_position = self.environment_positions["agent_env"]
-        grid_value = model.environments["value_env"].grid[current_position]
+        # grid_value = model.environments["value_env"].grid[current_position]
+        #
+        # print("Number of agents at poisition ({0}, {1}) is {2}".format(
+        #     current_position[0], current_position[1], grid_value))
 
-
-        print("Number of agents at poisition ({0}, {1}) is {2}".format(
-            current_position[0], current_position[1], grid_value))
+        number_of_agents_at_patch = model.environments["agent_env"].patches[current_position[0]][current_position[1]].number_of_agents_at_patch()
+        print("Number of agents at patch ({0}, {1}) is {2}".format(
+            current_position[0], current_position[1], number_of_agents_at_patch))
 
         self.__move_to_next_position(model)
 
     def __move_to_next_position(self, model):
-        """
-        Moves the agent to the next position in the grid. Movement occurs
-        along the x-axis, once the end of the x-axis is reached it proceeds
-        to th next y-axis position until the end of the grid is reached.
-        Then the agent stops.
-        Parameters
-        ----------
-        model : Model
-            The current model instance
-        """
-
-        if self.end_of_grid:
-            return
 
         current_position = self.environment_positions["agent_env"]
 
         xlimit = model.environments["agent_env"].xsize - 1
         ylimit = model.environments["agent_env"].ysize - 1
 
-        if current_position == (xlimit, ylimit):
-            self.end_of_grid = True
-            return
-
         # return a shuffled random list of the adjacent positions
         adjacentPositions = Region.Region.get_moore_neighbourhood(model.environments["agent_env"], current_position, True)
 
+        # if it is a valid position to move to
         if (model.environments["agent_env"].valid_position(adjacentPositions[0])):
             new_position = adjacentPositions[0]
 
+        # before moving, need to update the number of agents at that patch
+        model.environments["agent_env"].remove_agent_from_patch(new_position[0], new_position[1])
+        model.environments["agent_env"].add_agent_to_patch(new_position[0], new_position[1])
         self.move_agent("agent_env", new_position, model)
 
-
-xsize = ysize = 5
-
+xsize = ysize = 3
 model = Model(xsize * ysize + 5)
 
 # Creating the grid automatically binds it to the model
-Region.Region("agent_env", xsize, ysize, model)
+Region.Region("agent_env", xsize, ysize, model, 1, 1, 1)
 numerical_env = NumericalGrid2D("value_env", xsize, ysize, model)
 
 agents = []
-for x in range(0, 9):
+for x in range(0, 5):
     agents.append(SimpleAgent(model))
     model.schedule.agents.add(agents[x])
     numerical_env.grid[(0, 0)] += 1
+
+model.schedule.helpers.append(Region.RegionHelper())
 
 model.run()
--- Region.py
+++ Region.py
@@ -1,43 +1,80 @@
 from panaxea.core.Environment import ObjectGrid, ObjectGrid2D, NumericalGrid2D, Grid2D
+from panaxea.core.Steppables import Steppable, Helper
 
 class Region(ObjectGrid2D):
 
-    def __init__(self, name, xsize, ysize, model):
+    def __init__(self, name, xsize, ysize, model, R0, recovery_period, latency_period):
         super(Region, self).__init__(name, xsize, ysize, model)
+        self.global_num_susceptible = 0
+        self.global_num_infected = 0
+        self.global_num_exposed = 0
+        self.global_num_immune = 0
+        self.global_num_incidence = 0  # population to be converted to exposed
+        self.global_number_of_agents = 0
 
-        #self.patches = list()
+        self.SEIR_beta = R0 / recovery_period
+
+        if (latency_period == 0):
+            SEIR_lambda =  0
+        else:
+            SEIR_lambda = 1 / latency_period
+
+        self.SEIR_gamma = 1 / recovery_period
+
 
         # matrix of patches
-        self.patches = [[Patch.__init__(self, x, y) for x in range(xsize)] for y in range(ysize)]
-
-        # for x in range(xsize):
-        #     for y in range(ysize):
-        #         self.patches.insert()
-        #         self.patches.append(Patch.__init__(self, x, y))
+        self.patches = [[Patch(x, y) for x in range(xsize)] for y in range(ysize)]
 
     def add_agent_to_patch(self, x, y):
-        self.patches[x][y].Patch.increment_patch_agents()
-        print("PRINTING PATCH X, Y:", self.patches[x][y].number_of_agents)
+        self.patches[x][y].increment_patch_agents()
+
+    def remove_agent_from_patch(self, x, y):
+        self.patches[x][y].decrement_patch_agents()
 
     def number_of_patches(self):
         rows = len(self.patches)
         columns = len(self.patches[0])
         return rows * columns
 
-class Patch():
+    def print_out_region_patches(self):
+        rows = len(self.patches)
+        columns = len(self.patches[0])
+        for x in range(columns):
+            for y in range(rows):
+                print("[",self.patches[x][y].number_of_agents_at_patch(),"],", end="")
+                if y == (columns - 1):
+                    print(" ")
+
+class RegionHelper(Helper):
+
+    def __init__(self):
+        super(Helper, self).__init__()
+
+    def step_epilogue(self, model):
+        agent_env = model.environments["agent_env"]
+        model.environments["agent_env"].print_out_region_patches()
+
+
+class Patch(Steppable):
 
     def __init__(self, xcoord, ycoord):
         self.x = xcoord
         self.y = ycoord
-        self.susceptible = 0
-        self.infected = 0
-        self.recovered = 0
+        self.num_susceptible = 0
+        self.num_infected = 0
+        self.num_exposed = 0
+        self.num_immune = 0
+        self.num_incidence = 0 # population to be converted to exposed
         self.number_of_agents = 0
-
-        return self
 
     def increment_patch_agents(self):
         self.number_of_agents += 1
+
+    def decrement_patch_agents(self):
+        self.number_of_agents -= 1
+
+    def number_of_agents_at_patch(self):
+        return self.number_of_agents
 
     # calculated the number of new exposures / infections and allocate to patches
     def make_infections(self):
@@ -62,3 +99,56 @@ class Patch():
         dRdt = gamma * I
 
         return dSdt, dEdt, dIdt, dRdt
+
+    # SEIR_beta force of infection in epidemic model (excluding protective behaviour)
+    # SEIR_lambda transition rate from E to I
+    # SEIR_gamma transition rate from I to R
+    def update_SEIR_patches(self, SEIR_gamma, SEIR_lambda):
+
+        self.num_immune = self.num_immune + (SEIR_gamma * self.num_infected)
+
+        if (SEIR_gamma > 0):
+            self.num_infected = ((1 - SEIR_gamma) * self.num_infected) + (SEIR_lambda * self.num_exposed)
+
+            self.num_exposed = (1 - SEIR_lambda) * self.num_exposed + self.num_incidence
+
+            if (self.num_exposed < 1):
+                num_exposed = 0
+        else:
+            self.num_infected = ((1 - SEIR_gamma) * self.num_infected) + num_incidence
+
+            self.num_exposed = 0
+
+            if (self.num_infected < 1):
+                self.num_infected = 0
+
+        self.num_susceptible = self.num_susceptible - self.num_incidence
+
+        # def update_SEIR_patches(self, SEIR_gamma, SEIR_lambda, num_immune, num_infected, num_exposed, num_incidence,
+        #                         num_susceptible):
+        #
+        #     num_immune = num_immune + (SEIR_gamma * num_infected)
+        #
+        #     if (SEIR_gamma > 0):
+        #         num_infected = ((1 - SEIR_gamma) * num_infected) + (SEIR_lambda * num_exposed)
+        #
+        #         num_exposed = (1 - SEIR_lambda) * num_exposed + num_incidence
+        #
+        #         if (num_exposed < 1):
+        #             num_exposed = 0
+        #     else:
+        #         num_infected = ((1 - SEIR_gamma) * num_infected) + num_incidence
+        #
+        #         num_exposed = 0
+        #
+        #         if (num_infected < 1):
+        #             num_infected = 0
+        #
+        #     num_susceptible = num_susceptible - num_incidence
+
+    #     ; calculate discounted cumulative incidence
+    #       NEED TO IMPLEMENT
+
+    # def step_main(self):
+
+
